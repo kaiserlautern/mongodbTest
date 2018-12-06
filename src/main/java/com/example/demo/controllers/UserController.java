e371e6804a6daf6e01bf3c9ed7e1822c70488f36
César Moura <kaiserlautern@free.fr>
2018-12-06 10:46:46 -0300

    conectando ao mongodb via repository e service

---
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.User;
+import com.example.demo.services.UserService;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserController {
+	
+	@Autowired
+	private UserService service;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll(){
+		List<User> users = service.findAll();
+		return ResponseEntity.ok().body(users);
+	}
+}

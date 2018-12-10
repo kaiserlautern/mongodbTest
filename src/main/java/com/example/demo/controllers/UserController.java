a7f44474943d5235da51a0deae7a11aceb6d3bc2
kaiserlautern <kaiserlautern@free.fr>
2018-12-09 22:42:14 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -5,6 +5,8 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,4 +28,23 @@ public class UserController {
 		List<UserDTO> usersDto = users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(usersDto);
 	}
+	
+	@RequestMapping(method=RequestMethod.GET,value= "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		UserDTO userDto = new UserDTO(service.findById(id));
+		return ResponseEntity.ok().body(userDto);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<UserDTO> insert(@PathVariable String id){
+		UserDTO userDto = new UserDTO(service.findById(id));
+		return ResponseEntity.ok().body(userDto);
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,12 +1,15 @@
 package com.example.demo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.User;
+import com.example.demo.dto.UserDTO;
 import com.example.demo.repositories.UserRepository;
+import com.example.demo.services.exceptions.ObjectNotFoundException;
 
 
 @Service
@@ -14,8 +17,27 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repo;
+	
+	public User findById(String id) {
+		Optional<User> optional = repo.findById(id);
+		return optional.orElseThrow(() -> new ObjectNotFoundException("objeto não encontrado"));
+	}
 
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(),userDTO.getNome(), userDTO.getEmail());
 	}
 }
